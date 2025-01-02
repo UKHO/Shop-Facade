572cfaa1eeb28a99fd729bd952ef35cafed02a61
ankush365 <ankus1501365@mastek.com>
2025-01-02 17:15:50 +0530

    AB#195729 Added structure for FTs

---
+++ UKHO.ShopFacade.API.FunctionalTests/Configuration/ShopFacadeConfiguration.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UKHO.ShopFacade.API.FunctionalTests.Configuration
+{
+    public class ShopFacadeConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string EndpointUrl { get; set; }
+       
+    }
+}
