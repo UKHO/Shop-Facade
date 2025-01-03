0c682d7fd844c968ff0ce201fef1d010dc2b6c36
amogh14893 <84020354+amogh14893@users.noreply.github.com>
2025-01-03 18:46:05 +0530

    Project Template, UPN endpoint and Continuous Integration Pipeline for Shop Facade (#9)
    * AB#195729  Added Project Skeleton
    
    * 195729 Correlation Id & Exception Handling middleware added #AB195729 #AB195887 #AB195888
    
    * 195732 checking compatibility with changes for global.json AB#195731
    
    * AB#195729 Added key vault setup changes
    
    * 195731 added assembly version ps file AB#195731
    
    * AB#195729 - Added logger code changes
    
    * 195731 - changed coverage threshold limit % AB#195731
    
    * AB#195731 Moved FT project in test folder
    
    ---------
    
    Co-authored-by: ajay15273 <ajay15273@mastek.com>
    Co-authored-by: Pooja More <pooja15131@mastek.com>
    Co-authored-by: Amogh Lanke <Amogh14893@Mastek.Com>
    Co-authored-by: Prasad Rasal <Prasad14878@Mastek.Com>
    Co-authored-by: Surendra Behara <surendr11609@mastek.com>
    Co-authored-by: Pratibha Shinde <Pratibh14837@Mastek.Com>
    Co-authored-by: ankush365 <ankus1501365@mastek.com>
    Co-authored-by: Gaurav Mejari <gaurav15233@mastek.com>

---
+++ src/UKHO.ShopFacade.API/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UKHO.ShopFacade.Common.Constants;
+
+namespace UKHO.ShopFacade.API.Middleware
+{
+    [ExcludeFromCodeCoverage]
+    public class CorrelationIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CorrelationIdMiddleware> _logger;
+        public CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            var correlationId = httpContext.Request.Headers[ApiHeaderKeys.XCorrelationIdHeaderKey].FirstOrDefault();
+
+            if (string.IsNullOrEmpty(correlationId))
+            {
+                correlationId = Guid.NewGuid().ToString();
+                httpContext.Request.Headers.Append(ApiHeaderKeys.XCorrelationIdHeaderKey, correlationId);
+            }
+
+            httpContext.Response.Headers.Append(ApiHeaderKeys.XCorrelationIdHeaderKey, correlationId);
+
+            var state = new Dictionary<string, object>
+            {
+                [ApiHeaderKeys.XCorrelationIdHeaderKey] = correlationId!,
+            };
+
+            using (_logger.BeginScope(state))
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+}
---
+++ src/UKHO.ShopFacade.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using UKHO.ShopFacade.Common.Constants;
+using UKHO.ShopFacade.Common.Events;
+using UKHO.ShopFacade.Common.Exceptions;
+
+namespace UKHO.ShopFacade.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (ShopFacadeException shopFacadeException)
+            {
+                await HandleExceptionAsync(httpContext, shopFacadeException, shopFacadeException.EventId, shopFacadeException.Message, shopFacadeException.MessageArguments);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(httpContext, exception, EventIds.UnhandledException.ToEventId(), exception.Message);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception exception, EventId eventId, string message, params object[] messageArgs)
+        {
+            httpContext.Response.ContentType = ApiHeaderKeys.ContentType;
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            _logger.LogError(eventId, exception, message, messageArgs);
+
+            var correlationId = httpContext.Request.Headers[ApiHeaderKeys.XCorrelationIdHeaderKey].FirstOrDefault()!;
+            var problemDetails = new ProblemDetails
+            {
+                Extensions =
+                {
+                    ["correlationId"] = correlationId
+                }
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
