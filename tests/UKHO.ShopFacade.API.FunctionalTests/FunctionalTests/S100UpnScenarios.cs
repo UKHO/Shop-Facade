a53c31cd1f74e4b296003b1e2391121d38866951
ankush365 <ankus1501365@mastek.com>
2025-01-03 16:56:58 +0530

    AB#195729 Removed Auth folder

--- tests/UKHO.ShopFacade.API.FunctionalTests/FunctionalTests/S100UpnScenarios.cs
+++ tests/UKHO.ShopFacade.API.FunctionalTests/FunctionalTests/S100UpnScenarios.cs
@@ -1,5 +1,4 @@
-﻿using UKHO.ShopFacade.API.FunctionalTests.Auth;
-using UKHO.ShopFacade.API.FunctionalTests.Configuration;
+﻿using UKHO.ShopFacade.API.FunctionalTests.Configuration;
 using UKHO.ShopFacade.API.FunctionalTests.Service;
 
 namespace UKHO.ShopFacade.API.FunctionalTests.FunctionalTests
@@ -7,12 +6,10 @@ namespace UKHO.ShopFacade.API.FunctionalTests.FunctionalTests
     [TestFixture]
     public class S100UpnScenarios : TestFixtureBase
     {
-        private AuthTokenProvider _authTokenProvider;
         private S100UpnEndpoint _s100UpnEndpoint;
 
         public S100UpnScenarios()
         {
-            _authTokenProvider = new AuthTokenProvider();
             _s100UpnEndpoint = new S100UpnEndpoint();
         }
 
