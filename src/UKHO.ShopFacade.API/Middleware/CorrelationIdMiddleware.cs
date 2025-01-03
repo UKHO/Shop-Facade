1b673ae86d3a4491c87088454e710ad3a03a473f
Gaurav Mejari <gaurav15233@mastek.com>
2025-01-03 18:23:19 +0530

    AB#195731 Resolved review comment related to correlationIdMiddleware

--- src/UKHO.ShopFacade.API/Middleware/CorrelationIdMiddleware.cs
+++ src/UKHO.ShopFacade.API/Middleware/CorrelationIdMiddleware.cs
@@ -7,10 +7,11 @@ namespace UKHO.ShopFacade.API.Middleware
     public class CorrelationIdMiddleware
     {
         private readonly RequestDelegate _next;
-
-        public CorrelationIdMiddleware(RequestDelegate next)
+        private readonly ILogger<CorrelationIdMiddleware> _logger;
+        public CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext httpContext)
@@ -30,8 +31,7 @@ namespace UKHO.ShopFacade.API.Middleware
                 [ApiHeaderKeys.XCorrelationIdHeaderKey] = correlationId!,
             };
 
-            var logger = httpContext.RequestServices.GetRequiredService<ILogger<CorrelationIdMiddleware>>();
-            using (logger.BeginScope(state))
+            using (_logger.BeginScope(state))
             {
                 await _next(httpContext);
             }
