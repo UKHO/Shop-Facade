15b00630fba46565ac2eee21d5d21edfcad59f9e
Singaraiah Kokkera <singa1501799@mastek.com>
2025-01-13 19:07:14 +0530

    AB#196135 updated the runtest parameter and removing the sku name log_analytics

--- Deployment/Modules/AppInsights/main.tf
+++ Deployment/Modules/AppInsights/main.tf
@@ -2,7 +2,6 @@ resource "azurerm_log_analytics_workspace" "logs_analytics_workspace" {
   name                = "${var.name}-workspace"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
   retention_in_days   = 30
   tags                = var.tags
 }
