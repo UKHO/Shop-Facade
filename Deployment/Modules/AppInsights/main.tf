28a8acdbd2c1a834a2ca98597a7cfc823eb6c1b0
Singaraiah Kokkera <singa1501799@mastek.com>
2025-01-13 19:34:48 +0530

    Ab#196135 removing the azurerm_log_analytics_workspace resouces

--- Deployment/Modules/AppInsights/main.tf
+++ Deployment/Modules/AppInsights/main.tf
@@ -1,16 +1,7 @@
-resource "azurerm_log_analytics_workspace" "logs_analytics_workspace" {
-  name                = "${var.name}-workspace"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  retention_in_days   = 30
-  tags                = var.tags
-}
-
 resource "azurerm_application_insights" "app_insights" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.logs_analytics_workspace.id
   tags                = var.tags
 }
