b8749e1a0b189f5aec809ec15c580b20bc439e88
Pooja More <pooja15131@mastek.com>
2025-01-15 13:48:36 +0530

    AB#196132 Changes for authentication DI in graph client

--- src/UKHO.ShopFacade.Common/ClientProvider/GraphClient.cs
+++ src/UKHO.ShopFacade.Common/ClientProvider/GraphClient.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using Azure.Identity;
 using Microsoft.Extensions.Options;
 using Microsoft.Graph;
 using Microsoft.Graph.Models;
+using Microsoft.Kiota.Abstractions.Authentication;
 using UKHO.ShopFacade.Common.Configuration;
 
 namespace UKHO.ShopFacade.Common.ClientProvider
@@ -11,15 +11,19 @@ namespace UKHO.ShopFacade.Common.ClientProvider
     public class GraphClient : IGraphClient
     {
         private readonly IOptions<SharePointSiteConfiguration> _sharePointSiteConfiguration;
+        private readonly IOptions<GraphApiConfiguration> _graphApiConfiguration;
+        private readonly IAuthenticationProvider _authenticationProvider;
 
-        public GraphClient(IOptions<SharePointSiteConfiguration> sharePointSiteConfiguration)
+        public GraphClient(IOptions<SharePointSiteConfiguration> sharePointSiteConfiguration, IAuthenticationProvider authenticationProvider, IOptions<GraphApiConfiguration> graphApiConfiguration)
         {
             _sharePointSiteConfiguration = sharePointSiteConfiguration ?? throw new ArgumentNullException(nameof(sharePointSiteConfiguration));
+            _authenticationProvider = authenticationProvider;
+            _graphApiConfiguration = graphApiConfiguration;
         }
 
         public async Task<ListItemCollectionResponse> GetListItemCollectionResponse(string expandFields, string filterCondition)
         {
-            var graphClient = new GraphServiceClient(new DefaultAzureCredential(), new[] { "https://graph.microsoft.com/.default" });
+            var graphClient = new GraphServiceClient(_authenticationProvider, _graphApiConfiguration.Value.GraphApiBaseUrl);
 
             var listItemCollectionResponse = await graphClient.Sites[_sharePointSiteConfiguration.Value.SiteId]
                .Lists[_sharePointSiteConfiguration.Value.ListId]
