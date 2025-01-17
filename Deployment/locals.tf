209e309983163a8fba7437360a1b3e6351cb5e48
Amogh Lanke <Amogh14893@Mastek.Com>
2025-01-17 17:07:47 +0530

    19635 corrected changes in key vault name AB#19635

--- Deployment/locals.tf
+++ Deployment/locals.tf
@@ -2,7 +2,7 @@ locals {
   env_name           = lower(terraform.workspace)
   service_name       = "shopfacade"  
   web_app_name       = "${local.service_name}-${local.env_name}-api-webapp"
-  key_vault_name     = "${local.service_name}-$(local.env_name)-kv"
+  key_vault_name     = "${local.service_name}-${local.env_name}-kv"
   tags = {
     SERVICE                   = "SHOP Facade"
     ENVIRONMENT               = local.env_name
