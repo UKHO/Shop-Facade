ebf5dff786b0cd98b5a97b310efeb3e2655981ad
Singaraiah Kokkera <singa1501799@mastek.com>
2025-01-17 09:32:30 +0530

    Update the output.tf file

--- Deployment/Modules/Webapp/main.tf
+++ Deployment/Modules/Webapp/main.tf
@@ -16,8 +16,7 @@ resource "azurerm_linux_web_app" "webapp_service" {
 
   site_config {
     application_stack {    
-      current_stack  = "dotnet"
-      dotnet_version = "v8.0"
+      dotnet_version = "8.0"
     }
     always_on  = true
     ftps_state = "Disabled"
