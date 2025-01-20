ae0e623f11fa6644060f2152018c00a63cba75b4
Singaraiah Kokkera <singa1501799@mastek.com>
2025-01-20 15:32:34 +0530

    19635 Fix the review comments AB#19635

--- Deployment/locals.tf
+++ Deployment/locals.tf
@@ -4,12 +4,12 @@ locals {
   web_app_name       = "${local.service_name}-${local.env_name}-api-webapp"
   key_vault_name     = "${local.service_name}-${local.env_name}-kv"
   tags = {
-    SERVICE                   = "SHOP Facade"
+    SERVICE                   = "Shop Facade"
     ENVIRONMENT               = local.env_name
     SERVICE_OWNER             = "UKHO"
     RESPONSIBLE_TEAM          = "Abzu"
     CALLOUT_TEAM              = "On-Call_N/A"
-    COST_CENTRE               = "011.05.12"
+    COST_CENTRE               = "A.011.15.12"
     }
 }
 
