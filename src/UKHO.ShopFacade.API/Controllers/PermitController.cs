ece6e2bb927bd7a2e4ae81e251593903edb44456
Prasad Rasal <Prasad14878@Mastek.Com>
2025-02-19 14:02:07 +0530

    minor code changes AB#203375

--- src/UKHO.ShopFacade.API/Controllers/PermitController.cs
+++ src/UKHO.ShopFacade.API/Controllers/PermitController.cs
@@ -19,20 +19,20 @@ namespace UKHO.ShopFacade.API.Controllers
         }
 
         /// <summary>
-        /// Get a zip file containing all the S-100 permit file(s) of the requested licence..
+        /// Get a zip file containing all the S-100 permit file(s) of the requested licence.
         /// </summary>
         /// <param name="licenceId" example="12345678">Requested licence id.</param>
         /// <response code="200">OK - Returns a zip containing permit files</response>
-        /// <response code="401">Unauthorised - either you have not provided valid token, or your token is not recognised.</response>
+        /// <response code="401">Unauthorized - either you have not provided valid token, or your token is not recognized.</response>
         /// <response code="403">Forbidden - you have no permission to use this API.</response>
         [HttpGet]
         [Route("/v1/licences/{licenceId}/s100/permits")]
         [Authorize(Policy = ShopFacadeConstants.ShopFacadePermitPolicy)]
         [SwaggerOperation(Tags = new[] { "Licensing" }, Description = "<p>Returns a zip file containing all the S-100 permit file(s) of the requested licence.</p>")]
         [SwaggerResponse(statusCode: (int)HttpStatusCode.OK, type: typeof(string), description: "<p>OK - Returns a zip containing permit files</p>")]
-        [SwaggerResponse(statusCode: (int)HttpStatusCode.Unauthorized, description: "<p>Unauthorised - either you have not provided valid token, or your token is not recognised.</p>")]
+        [SwaggerResponse(statusCode: (int)HttpStatusCode.Unauthorized, description: "<p>Unauthorized - either you have not provided valid token, or your token is not recognized.</p>")]
         [SwaggerResponse(statusCode: (int)HttpStatusCode.Forbidden, description: "<p>Forbidden - you have no permission to use this API.</p>")]
-        public IActionResult GetPermits([SwaggerParameter(Required = true)] int licenceId)
+        public async Task<IActionResult> GetPermits([SwaggerParameter(Required = true)] int licenceId)
         {
             _logger.LogInformation(EventIds.GetPermitsCallStarted.ToEventId(), "GetPermits API Call Started.");
 
