9a4d7c8bf02cb84e1b48d762d64f93e4c387dd18
ankush365 <ankus1501365@mastek.com>
2025-02-25 19:28:50 +0530

    AB#203375 Resolved review comments

--- tests/UKHO.ShopFacade.API.FunctionalTests/Configuration/ShopFacadeConfiguration.cs
+++ tests/UKHO.ShopFacade.API.FunctionalTests/Configuration/ShopFacadeConfiguration.cs
@@ -4,8 +4,8 @@ namespace UKHO.ShopFacade.API.FunctionalTests.Configuration
     public class ShopFacadeConfiguration
     {
         public string? BaseUrl { get; set; }
-        public string? getS100UpnEndpoint { get; set; }
-        public string? getS100PermitEndpoint { get; set; }
+        public string? UpnEndpoint { get; set; }
+        public string? PermitEndpoint { get; set; }
        
     }
 }
--- tests/UKHO.ShopFacade.API.FunctionalTests/FunctionalTests/PermitScenarios.cs
+++ tests/UKHO.ShopFacade.API.FunctionalTests/FunctionalTests/PermitScenarios.cs
@@ -6,43 +6,43 @@ using UKHO.ShopFacade.API.FunctionalTests.Service;
 namespace UKHO.ShopFacade.API.FunctionalTests.FunctionalTests
 {
     [TestFixture]
-    public class S100PermitScenarios : TestFixtureBase
+    public class PermitScenarios : TestFixtureBase
     {
-        private readonly S100PermitEndpoint _s100PermitEndpoint;
+        private readonly PermitEndpoint _permitEndpoint;
 
         private readonly AuthTokenProvider _authTokenProvider;
 
-        public S100PermitScenarios()
+        public PermitScenarios()
         {
-            _s100PermitEndpoint = new S100PermitEndpoint();
+            _permitEndpoint = new PermitEndpoint();
             _authTokenProvider = new AuthTokenProvider();
         }
 
         [Test]
         public async Task WhenPermitServiceEndpointCalledWithValidTokenAndLicenceId_ThenPermitServiceReturns200OkResponse()
         {
-            var response = await _s100PermitEndpoint.GetUpnResponseAsync(await _authTokenProvider.GetAzureADTokenAsync(false), "1");
+            var response = await _permitEndpoint.GetUpnResponseAsync(await _authTokenProvider.GetAzureADTokenAsync(false), "1");
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
 
         [Test]
         public async Task WhenPermitServiceEndpointCalledWithValidTokenWithNoRole_ThenPermitServiceReturns403ForbiddenResponse()
         {
-            var response = await _s100PermitEndpoint.GetUpnResponseAsync(await _authTokenProvider.GetAzureADTokenAsync(true), "1");
+            var response = await _permitEndpoint.GetUpnResponseAsync(await _authTokenProvider.GetAzureADTokenAsync(true), "1");
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
         }
 
         [Test]
         public async Task WhenPermitServiceEndpointCalledWithoutToken_ThenPermitServiceReturns401UnauthorizedResponse()
         {
-            var response = await _s100PermitEndpoint.GetUpnResponseAsync("", "1");
+            var response = await _permitEndpoint.GetUpnResponseAsync("", "1");
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
         }
 
         [Test]
         public async Task WhenPermitServiceEndpointCalledWithInvalidToken_ThenPermitServiceReturns401UnauthorizedResponse()
         {
-            var response = await _s100PermitEndpoint.GetUpnResponseAsync("Invalid Token", "1");
+            var response = await _permitEndpoint.GetUpnResponseAsync("Invalid Token", "1");
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
         }
 
