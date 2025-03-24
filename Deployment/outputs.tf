7ef2552ca330ecf5252f0ec01bfa2e0dd6510338
Amogh Lanke <Amogh14893@Mastek.Com>
2025-03-24 11:52:50 +0530

    207357 changes for addsmock webapp name as output variable AB#207357

--- Deployment/outputs.tf
+++ Deployment/outputs.tf
@@ -3,7 +3,7 @@ output "webapp_name" {
 }
 
 output "adds_mock_webapp_name" {
-  value =  module.webapp_service.adds_mock_webapp_name
+  value = local.env_name == "dev" ? local.adds_mock_web_app_name : null
 }
 
 output "resource_group" {
