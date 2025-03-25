7e6c9ed75395c7074a075a23a8c1c523f57550e9
ajay15273 <ajay15273@mastek.com>
2025-03-25 21:17:18 +0530

    AB#211925 Minor change

--- src/UKHO.ShopFacade.Common/Constants/ErrorDetails.cs
+++ src/UKHO.ShopFacade.Common/Constants/ErrorDetails.cs
@@ -32,12 +32,12 @@ namespace UKHO.ShopFacade.Common.Constants
         public const string ScsInternalErrorMessage = "Error occurred while processing request from {ScsSource}.";
         public const string GetSalesCatalogueDataRequestStartedMessage = "Retrieval process of the latest S-100 basic catalogue data from {ScsSource} is started.";
         public const string GetSalesCatalogueDataRequestCompletedMessage = "Retrieval process of the latest S-100 basic catalogue data from {ScsSource} is completed.";
-        public const string SalesCatalogueDataRequestInternalServerErrorMessage = "Error occurred while processing {ScsSource} request | StatusCode: {StatusCode} | Uri: {RequestUri} | Error:{errorResponse}.";
+        public const string SalesCatalogueDataRequestInternalServerErrorMessage = "Error occurred while processing {ScsSource} request | StatusCode: {StatusCode} | Uri: {RequestUri} | Error: {errorResponse}.";
 
         //Error messages for S100 permit service
         public const string S100PermitServiceSource = "S100 permit service";
         public const string GetS100PermitServiceRequestStartedMessage = "Retrieval process of the permit data from {S100PermitServiceSource} is started.";
         public const string GetS100PermitServiceRequestCompletedMessage = "Retrieval process of the permit data from {S100PermitServiceSource} is completed.";
-        public const string S100PermitServiceInternalServerErrorMessage = "Error occurred while processing {S100PermitServiceSource} request | StatusCode: {StatusCode} | Uri: {RequestUri} | Error:{errorResponse}.";
+        public const string S100PermitServiceInternalServerErrorMessage = "Error occurred while processing {S100PermitServiceSource} request | StatusCode: {StatusCode} | Uri: {RequestUri} | Error: {errorResponse}.";
     }
 }
