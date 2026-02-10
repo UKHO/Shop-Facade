434589dde792f5f112717e144fa570a7a032fb05
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2026-02-10 12:08:08 +0000

    Revert required Terraform version to 1.9.2 for compatibility

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -5,7 +5,7 @@ terraform {
       version = "=4.14.0"
     }
   }
-  required_version = "=1.10.4"
+  required_version = "=1.9.2"
   backend "azurerm" {
     key                  = "terraform.deployment.tfplan"
     container_name       = "tfstate"
