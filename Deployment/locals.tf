67d0eede6040a27a2fc7eaab465d8147a42e1025
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2026-02-23 11:29:20 +0000

    create private endpoints for KV and storage account so pipeline agents can access them

--- Deployment/locals.tf
+++ Deployment/locals.tf
@@ -12,9 +12,23 @@ locals {
     CALLOUT_TEAM     = "On-Call_N/A"
     COST_CENTRE      = "A.011.15.12"
   }
-  pe_identity        = "${local.service_name}${local.env_name}"
-  vnet_link          = "${local.service_name}${local.env_name}"
-  private_connection = "/subscriptions/${var.subscription_id}/resourceGroups/shopfacade-${local.env_name}-rg/providers/Microsoft.Web/sites/shopfacade-${local.env_name}-api-webapp"
-  zone_group         = "${local.service_name}${local.env_name}zone"
+  pe_identity = "${local.service_name}${local.env_name}"
+  vnet_link   = "${local.service_name}${local.env_name}"
+
+  # Private endpoint connections
+  private_connection_webapp      = "/subscriptions/${var.subscription_id}/resourceGroups/shopfacade-${local.env_name}-rg/providers/Microsoft.Web/sites/shopfacade-${local.env_name}-api-webapp"
+  private_connection_keyvault    = "/subscriptions/${var.subscription_id}/resourceGroups/m-spokeconnect-rg/providers/Microsoft.KeyVault/vaults/${local.service_name}${local.env_name}-kv"
+  private_connection_keyvault_ex = "/subscriptions/${var.subscription_id}/resourceGroups/m-spokeconnect-rg/providers/Microsoft.KeyVault/vaults/${local.service_name}${local.env_name}-kv-ex"
+  private_connection_storage     = "/subscriptions/${var.subscription_id}/resourceGroups/m-spokeconnect-rg/providers/Microsoft.Storage/storageAccounts/shopfacade${local.env_name}sa"
+
+  # Zone groups for DNS integration
+  zone_group_webapp      = "${local.service_name}${local.env_name}webapp-zone"
+  zone_group_keyvault    = "${local.service_name}${local.env_name}kv-zone"
+  zone_group_keyvault_ex = "${local.service_name}${local.env_name}kv-ex-zone"
+  zone_group_storage     = "${local.service_name}${local.env_name}storage-zone"
+
+  # Legacy variables for backwards compatibility
+  private_connection = local.private_connection_webapp
+  zone_group         = local.zone_group_webapp
   dns_resource_group = var.dns_zone_rg
 }
--- Deployment/privateEndpoint.tf
+++ Deployment/privateEndpoint.tf
@@ -16,15 +16,77 @@ data "azurerm_subnet" "pesn" {
   resource_group_name  = var.spoke_rg
 }
 
-module "private_endpoint_link" {
+# Web App Private Endpoint
+module "private_endpoint_webapp" {
+  count  = contains(["prod", "pre"], local.env_name) ? 0 : 1
   source = "github.com/UKHO/tfmodule-azure-private-endpoint-private-link?ref=0.7.1"
   providers = {
     azurerm.hub   = azurerm.hub
     azurerm.spoke = azurerm.shopfacade
   }
-  private_connection          = [local.private_connection]
-  zone_group                  = local.zone_group
-  pe_identity                 = [local.pe_identity]
+  private_connection          = [local.private_connection_webapp]
+  zone_group                  = local.zone_group_webapp
+  pe_identity                 = ["${local.pe_identity}-webapp"]
+  pe_environment              = local.env_name
+  pe_vnet_rg                  = var.spoke_rg
+  pe_vnet_name                = var.pe_vnet_name
+  pe_subnet_name              = var.pe_subnet_name
+  pe_resource_group           = [azurerm_resource_group.rg.name]
+  dns_resource_group          = local.dns_resource_group
+  pe_resource_group_locations = [azurerm_resource_group.rg.location]
+}
+
+# Key Vault Private Endpoint
+module "private_endpoint_keyvault" {
+  count  = contains(["prod", "pre"], local.env_name) ? 0 : 1
+  source = "github.com/UKHO/tfmodule-azure-private-endpoint-private-link?ref=0.7.1"
+  providers = {
+    azurerm.hub   = azurerm.hub
+    azurerm.spoke = azurerm.shopfacade
+  }
+  private_connection          = [local.private_connection_keyvault]
+  zone_group                  = local.zone_group_keyvault
+  pe_identity                 = ["${local.pe_identity}-kv-tf"]
+  pe_environment              = local.env_name
+  pe_vnet_rg                  = var.spoke_rg
+  pe_vnet_name                = var.pe_vnet_name
+  pe_subnet_name              = var.pe_subnet_name
+  pe_resource_group           = [azurerm_resource_group.rg.name]
+  dns_resource_group          = local.dns_resource_group
+  pe_resource_group_locations = [azurerm_resource_group.rg.location]
+}
+
+# Key Vault External Private Endpoint
+module "private_endpoint_keyvault_ex" {
+  count  = contains(["prod", "pre"], local.env_name) ? 0 : 1
+  source = "github.com/UKHO/tfmodule-azure-private-endpoint-private-link?ref=0.7.1"
+  providers = {
+    azurerm.hub   = azurerm.hub
+    azurerm.spoke = azurerm.shopfacade
+  }
+  private_connection          = [local.private_connection_keyvault_ex]
+  zone_group                  = local.zone_group_keyvault_ex
+  pe_identity                 = ["${local.pe_identity}-kv-ex-tf"]
+  pe_environment              = local.env_name
+  pe_vnet_rg                  = var.spoke_rg
+  pe_vnet_name                = var.pe_vnet_name
+  pe_subnet_name              = var.pe_subnet_name
+  pe_resource_group           = [azurerm_resource_group.rg.name]
+  dns_resource_group          = local.dns_resource_group
+  pe_resource_group_locations = [azurerm_resource_group.rg.location]
+}
+
+# Storage Account Private Endpoint
+module "private_endpoint_storage" {
+  count  = contains(["prod", "pre"], local.env_name) ? 0 : 1
+  source = "github.com/UKHO/tfmodule-azure-private-endpoint-private-link?ref=0.7.1"
+  providers = {
+    azurerm.hub   = azurerm.hub
+    azurerm.spoke = azurerm.shopfacade
+  }
+  private_connection          = [local.private_connection_storage]
+  zone_group                  = local.zone_group_storage
+  pe_identity                 = ["${local.pe_identity}-storage-tf"]
   pe_environment              = local.env_name
   pe_vnet_rg                  = var.spoke_rg
   pe_vnet_name                = var.pe_vnet_name
