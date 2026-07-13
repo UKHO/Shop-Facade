d705acc633169c64ba3a43ad73188f917e6aa4dc
Amrendra Pandey <amren1502129@mastek.com>
2026-07-13 13:58:57 +0530

    AB#275597-devops related changes

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -2,28 +2,32 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.14.0"
+      version = ">= 4.80.0"
     }
   }
   required_version = "=1.10.4"
   backend "azurerm" {
     key            = "terraform.deployment.tfplan"
     container_name = "tfstate"
+    version              = ">= 4.80.0"
   }
 }
 
 provider "azurerm" {
   features {}
+  version              = ">= 4.80.0"
 }
 
 provider "azurerm" {
   features {}
   alias           = "hub"
   subscription_id = var.hub_subscription_id
+   version              = ">= 4.80.0"
 }
 
 provider "azurerm" {
   features {}
   alias           = "shopfacade"
   subscription_id = var.subscription_id
+  version              = ">= 4.80.0"
 }
