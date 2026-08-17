d5991e0e02863459635f7adad5e8e80827575de6
Amrendra Pandey <amren1502129@mastek.com>
2026-08-17 17:20:33 +0530

    AB#275597-dotnet 10  upgrade dev and devops  changes

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.14.0"
+      version = "= 4.81.0"
     }
   }
-  required_version = "=1.10.4"
+  required_version = "=1.15.8"
   backend "azurerm" {
     key            = "terraform.deployment.tfplan"
     container_name = "tfstate"
